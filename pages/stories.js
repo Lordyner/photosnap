606f40a753aacd2ce4675e87af7dba5a839424bd
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2024-02-21 18:23:08 +0100

    Add missing imports

--- pages/stories.js
+++ pages/stories.js
@@ -1,67 +1,70 @@
-// import { getLogger } from "@/Logging/log-util";
-// import Head from "next/head";
+import { getLogger } from "@/Logging/log-util";
+import GlobalContext from "@/Store/GlobalContext";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 
-// export default function Stories() {
-//     /* Logger */
-//     const logger = getLogger('Stories');
-//     logger.debug('Stories page rendered');
+export default function Stories() {
+    /* Logger */
+    const logger = getLogger('Stories');
+    logger.debug('Stories page rendered');
 
-//     /* State */
-//     const [screenWidth, setScreenWidth] = useState();
+    /* State */
+    const [screenWidth, setScreenWidth] = useState();
 
-//     /* Context */
-//     const { setIsMobileResolution } = useContext(GlobalContext);
-//     const { setIsTabletResolution } = useContext(GlobalContext);
-//     const { setIsLaptopResolution } = useContext(GlobalContext);
-//     const { setIsDesktopResolution } = useContext(GlobalContext);
-//     const { tabletResolution, laptopResolution, desktopResolution } = useContext(GlobalContext);
-//     const { setIsLoading } = useContext(GlobalContext);
-//     const { isMenuOpen } = useContext(GlobalContext);
+    /* Context */
+    const { setIsMobileResolution } = useContext(GlobalContext);
+    const { setIsTabletResolution } = useContext(GlobalContext);
+    const { setIsLaptopResolution } = useContext(GlobalContext);
+    const { setIsDesktopResolution } = useContext(GlobalContext);
+    const { tabletResolution, laptopResolution, desktopResolution } = useContext(GlobalContext);
+    const { setIsLoading } = useContext(GlobalContext);
+    const { isMenuOpen } = useContext(GlobalContext);
 
-//     /* Router */
-//     const router = useRouter();
+    /* Router */
+    const router = useRouter();
 
-//     /* Functions */
-//     const handleMenuDisplay = () => {
-//         setScreenWidth(window.screen.width);
+    /* Functions */
+    const handleMenuDisplay = () => {
+        setScreenWidth(window.screen.width);
 
-//         const isMobile = screenWidth < tabletResolution;
-//         const isTablet = screenWidth >= tabletResolution && screenWidth < laptopResolution;
-//         const isLaptop = screenWidth >= laptopResolution && screenWidth < desktopResolution;
-//         const isDesktop = screenWidth >= desktopResolution;
+        const isMobile = screenWidth < tabletResolution;
+        const isTablet = screenWidth >= tabletResolution && screenWidth < laptopResolution;
+        const isLaptop = screenWidth >= laptopResolution && screenWidth < desktopResolution;
+        const isDesktop = screenWidth >= desktopResolution;
 
-//         setIsMobileResolution(isMobile);
-//         setIsTabletResolution(isTablet);
-//         setIsLaptopResolution(isLaptop);
-//         setIsDesktopResolution(isDesktop);
-//     }
+        setIsMobileResolution(isMobile);
+        setIsTabletResolution(isTablet);
+        setIsLaptopResolution(isLaptop);
+        setIsDesktopResolution(isDesktop);
+    }
 
-//     useEffect(() => {
-//         // Handle menu display
-//         handleMenuDisplay();
-//         window.addEventListener('resize', handleMenuDisplay);
+    useEffect(() => {
+        // Handle menu display
+        handleMenuDisplay();
+        window.addEventListener('resize', handleMenuDisplay);
 
-//         // Handle loading spinner
-//         router.events.on("routeChangeStart", () => setIsLoading(true));
-//         router.events.on("routeChangeComplete", () => setIsLoading(false));
-//         console.log()
-//     }, [screenWidth])
-
-
-//     return (
-//         <>
-//             <Head>
-//                 <title>MyTeam</title>
-//                 <meta name="description" content="Create and share your photo stories" />
-//                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-//                 <link rel="icon" href="/favicon.ico" />
-//             </Head>
-//             <div className={`overlay-burger-menu ${isMenuOpen ? 'isActive' : ''}`} />
-//             <div>
-//                 <h1>Stories page</h1>
-//             </div>
+        // Handle loading spinner
+        router.events.on("routeChangeStart", () => setIsLoading(true));
+        router.events.on("routeChangeComplete", () => setIsLoading(false));
+        console.log()
+    }, [screenWidth])
 
 
-//         </>
-//     )
-// }
+    return (
+        <>
+            <Head>
+                <title>MyTeam</title>
+                <meta name="description" content="Create and share your photo stories" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={`overlay-burger-menu ${isMenuOpen ? 'isActive' : ''}`} />
+            <div>
+                <h1>Stories page</h1>
+            </div>
+
+
+        </>
+    )
+}
